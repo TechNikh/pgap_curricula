7f43f2ed5454b882850d3e8888a9bfb871083ad0
Avinash Kalola <avinash_kalola@yahoo.com>
2017-01-12 19:53:36 +0530

    Add files via upload

--- www/js/database.js
+++ www/js/database.js
@@ -8,6 +8,7 @@ app.insertVideoRecord = function(name, path, extension) {
         [ name, path, extension ], app.onSuccess, app.onError);
   });
 }
+
 app.insertImageRecord = function(name, path, extension) {
   app.db.transaction(function(tx) {
     tx.executeSql(
@@ -30,6 +31,7 @@ app.insertYAMLrecord = function(name, path, extension, type, offline_file,
                     displayName, description ], app.onSuccess, app.onError);
       });
 }
+
 app.prepareCacheTables = function() {
   // Drop table
   app.db.transaction(function(tx) {
@@ -79,12 +81,49 @@ app.prepareCacheTables = function() {
 
 app.onSuccess = function(tx, r) {
   // console.log("Your SQLite query was successful!");
+  $.mobile.loading('hide');	
+}
+
+app.onSuccessWithCallback = function(tx, r,callback) {
+  // console.log("Your SQLite query was successful!");
+  callback();
+  $.mobile.loading('hide');	
 }
 
 app.onError = function(tx, e) {
+
   console.log("SQLite Error: " + JSON.stringify(e));
   // SQLite Error: {"rows":{"length":0},"rowsAffected":0}
   if (e.message) {
     navigator.notification.alert("SQLite Error: " + e.message);
   }
 }
+
+app.insertDiscussionPoints = function(title,question,callback){
+	//Create Discussion table if already not exists
+	$.mobile.loading('show');
+	app.db
+      .transaction(function(tx) {
+        tx
+            .executeSql(
+                'CREATE TABLE IF NOT EXISTS discussion_points (Id integer primary key AUTOINCREMENT, Discussion_Point text,Discussion_Title_Point text,Answer text)',
+                app.privateInsertDiscussionPoints(title,question,callback), app.onError);
+      });
+}
+
+app.privateInsertDiscussionPoints = function(title,question,callback){
+	app.db.transaction(function(tx) {
+		tx.executeSql(
+			"INSERT INTO discussion_points (Discussion_Title_Point,Discussion_Point) VALUES (?,?)",
+			[title,question], function(transaction, result){app.onSuccessWithCallback(transaction,result,callback);}, app.onError);
+	  });
+}
+
+app.updateDiscussionAnswer = function(id,answer){
+	app.db.transaction(function(tx) {
+		tx.executeSql(
+			"UPDATE discussion_points SET  Answer = ? WHERE Id = ?",
+			[answer,id], app.onSuccess, app.onError);
+	  });
+}
+
